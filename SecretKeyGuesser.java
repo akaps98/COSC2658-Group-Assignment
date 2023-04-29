6ce035d53cc2882510d5de436e896c9060fcec41
tjdwns <40561173+tjdwns@users.noreply.github.com>
2023-04-30 00:37:14 +0700

    Add files via upload

--- SecretKeyGuesser.java
+++ SecretKeyGuesser.java
@@ -7,55 +7,61 @@ public class SecretKeyGuesser {
         boolean isFront = true;
         int checkingIdx;
         int match = key.guess(str);
-        System.out.println("Guessing... " + str);
 
-        while (match != -1 && match != 16) {
-            if(isFront){
-                checkingIdx = idx1;
-            }else{
-                checkingIdx = idx2;
-            }
-
-            char[] curr = str.toCharArray();
-            int newMatch;
-            // Check if the program changed a character that already matches the correct key.
-            toNextChar(curr, checkingIdx); // 2
-            String s;
-            s = String.valueOf(curr);
-            System.out.println("Guessing... " + s);
-            newMatch = key.guess(s);
-            if (newMatch > match) {
-                match = newMatch;
-                str = s;
-            } else if(newMatch == match) {
-                toNextChar(curr, checkingIdx);
-                s = String.valueOf(curr);
-                newMatch = key.guess(s);
-                System.out.println("Guessing... " + s);
-                if(newMatch == match) {
-                    // Since there are only 4 characters available in order to create a key
-                    // The character in the current index must match the one in the secret key
-                    // When it moves once more to a next char
-                    toNextChar(curr, checkingIdx); // 3
-                    s = String.valueOf(curr);
-                    System.out.println("Guessing... " + s);
-                    newMatch++;
-                    if(newMatch == 16){
-                        key.guess(s);
-                    }
+        // Doesn't go through the loop if a user enters an invalid value.
+        // When it gets -1 from the 'guess' method
+        if(match == -1){
+            System.out.println("Invalid! Please enter a valid key!!");
+        } else {
+            System.out.println("Guessing... " + str);
+            while (match != 16) {
+                if (isFront) {
+                    checkingIdx = idx1;
+                } else {
+                    checkingIdx = idx2;
                 }
-                str = s;
-                match = newMatch;
+
+                char[] curr = str.toCharArray();
+                int newMatch;
+                // Check if the program changed a character that already matches the correct key.
+                toNextChar(curr, checkingIdx); // 2
+                String s;
+                s = String.valueOf(curr);
+                System.out.println("Guessing... " + s);
+                newMatch = key.guess(s);
+                if (newMatch > match) {
+                    match = newMatch;
+                    str = s;
+                } else if (newMatch == match) {
+                    toNextChar(curr, checkingIdx);
+                    s = String.valueOf(curr);
+                    newMatch = key.guess(s);
+                    System.out.println("Guessing... " + s);
+                    if (newMatch == match) {
+                        // Since there are only 4 characters available in order to create a key
+                        // The character in the current index must match the one in the secret key
+                        // When it moves once more to a next char
+                        toNextChar(curr, checkingIdx); // 3
+                        s = String.valueOf(curr);
+                        System.out.println("Guessing... " + s);
+                        newMatch++;
+                        if (newMatch == 16) {
+                            key.guess(s);
+                        }
+                    }
+                    str = s;
+                    match = newMatch;
+                }
+                if (isFront) {
+                    idx1++;
+                    isFront = false;
+                } else {
+                    idx2--;
+                    isFront = true;
+                }
             }
-            if(isFront){
-                idx1++;
-                isFront = false;
-            }else {
-                idx2--;
-                isFront = true;
-            }
+            System.out.println("I found the secret key. It is " + str);
         }
-        System.out.println("I found the secret key. It is " + str);
     }
 
     static void toNextChar(char[] curr, int idx) {
